b34d0a99e398b558691aab20b1daaa3dc4caa940
Sam Jones <sam@samljones.com>
2016-09-30 12:10:53 -0400

    Adds Fetch Commands for players

---
+++ src/Players/Commands.elm
@@ -0,0 +1,27 @@
+module Players.Commands exposing (..)
+
+import Http
+import Task
+import Json.Decode as Decode exposing ((:=))
+
+import Players.Messages exposing (..)
+import Players.Models exposing (..)
+
+fetchAll : Cmd Msg
+fetchAll =
+    Http.get collectionDecoder fetchAllUrl
+        |> Task.perform FetchAllFail FetchAllDone
+
+fetchAllUrl : String
+fetchAllUrl = "http://localhost:4000/players"
+
+collectionDecoder : Decode.Decoder (List Player)
+collectionDecoder =
+    Decode.list memberDecoder
+
+memberDecoder : Decode.Decoder Player
+memberDecoder =
+    Decode.object3 Player
+        ( "id" := Decode.int )
+        ( "name" := Decode.string )
+        ( "level" := Decode.int )
--- src/Players/Messages.elm
+++ src/Players/Messages.elm
@@ -1,4 +1,8 @@
 module Players.Messages exposing (..)
 
-type Msg =
-    NoOp
+import Http
+import Players.Models exposing (PlayerId, Player)
+
+type Msg
+    = FetchAllDone (List Player)
+    | FetchAllFail Http.Error
