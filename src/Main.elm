3e64b6e04b9ef53140c8dadffb39d3e1cad32897
Sam Jones <sam@samljones.com>
2016-09-29 23:18:27 -0400

    Adds elm build to project

---
+++ src/Main.elm
@@ -0,0 +1,33 @@
+module Main exposing (..)
+
+import Html.App
+
+import Messages exposing (Msg)
+import Models exposing (Model)
+import View exposing (view)
+import Update exposing (update)
+
+-- Init
+
+init : (Model, Cmd Msg)
+init =
+    ( "Hello", Cmd.none )
+
+
+-- Subscriptions
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+-- MAIN
+
+main : Program Never
+main =
+    Html.App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
